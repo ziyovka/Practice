06c135d2b5c21e2156dc3ab18f7996f9eaedf434
ziyovka <ziyovuddinjamoliddinov380@gmail.com>
2025-11-28 17:34:06 +0700

    Upload OOP tasks

---
+++ src/main/java/org/example/Car.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Car {
+    String color;
+    String model;
+    int price;
+    Car(String color,String model,int price){
+        this.color=color;
+        this.model=model;
+        this.price=price;
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+//import javax.sound.sampled.*;
+//import java.io.*;
+//import java.time.LocalDate;
+//import java.time.LocalDateTime;
+//import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+Car mycar=new Car("Black","Audi",75000);
+        System.out.println(mycar.price);
+        System.out.println(mycar.model);
+        System.out.println(mycar.color);
+    }
+}
